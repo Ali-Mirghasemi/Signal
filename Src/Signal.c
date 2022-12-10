c9ed901ff24f69ceef8155dbf103bffb59985497
Ali <ali.mirghasemi1376@gmail.com>
2022-12-10 11:49:59 +0330

    Initial commit

---
+++ Src/Signal.c
@@ -0,0 +1,278 @@
+#include "Signal.h"
+
+/* private variables */
+static const Signal_Driver* signalDriver;
+#if SIGNAL_MAX_NUM == -1
+    static Signal* lastSignal = SIGNAL_NULL;
+#else
+    static Signal signals[SIGNAL_MAX_NUM] = {0};
+#endif // SIGNAL_MAX_NUM == -1
+
+/**
+ * @brief use for initialize
+ * 
+ * @param driver 
+ */
+void Signal_init(const Signal_Driver* driver) {
+    signalDriver = driver;
+}
+/**
+ * @brief user must place it in timer with 20ms ~ 50ms 
+ * all of callbacks handle and fire in this function
+ */
+void Signal_handle(void) {
+    Signal_State state;
+#if SIGNAL_MAX_NUM == -1
+    Signal* pSignal = lastSignal;
+    while (SIGNAL_NULL != pSignal) {
+#else
+    Signal* pSignal = signals;
+    uint8_t len = SIGNAL_MAX_NUM;
+    while (len-- > 0) {
+        if (pSignal->Configured) {
+#endif
+    #if SIGNAL_ENABLE_FLAG
+        if (pSignal->Enabled) {
+    #endif // SIGNAL_ENABLE_FLAG
+        // update current state
+        state =(Signal_State) (((pSignal->State << 1) | signalDriver->readPin(pSignal->Config)) & 0x03);
+        pSignal->State = state;
+        // call callback on new state
+		if (pSignal->NotActive == Signal_NotHandled
+        #if SIGNAL_IDLE != -1
+            && SIGNAL_IDLE != state
+        #endif  
+            ) 
+    {
+        #if SIGNAL_MULTI_CALLBACK
+            if (pSignal->Callbacks.callbacks[state]) {
+            #if SIGNAL_IDLE != -1
+                pSignal->NotActive = 
+            #endif
+                pSignal->Callbacks.callbacks[state](pSignal, state);
+            }
+        #else
+            if (pSignal->Callbacks.onChange) {
+            #if SIGNAL_IDLE != -1
+                pSignal->NotActive = 
+            #endif
+                pSignal->Callbacks.onChange(pSignal, state);
+            }
+        #endif /* SIGNAL_MULTI_CALLBACK_ENABLE */
+        }
+    #if SIGNAL_IDLE != -1
+        else if (SIGNAL_IDLE == state && pSignal->NotActive != Signal_NotHandled) {
+            pSignal->NotActive = Signal_NotHandled;
+        }
+    #endif
+    #if SIGNAL_ENABLE_FLAG
+        }
+    #endif // Signal_ENABLE_FLAG
+    #if SIGNAL_MAX_NUM == -1
+        // switch to previous Signal
+        pSignal = pSignal->Previous;
+    #else
+        }
+        pSignal++;
+    #endif // SIGNAL_MAX_NUM == -1
+    }
+}
+
+/**
+ * @brief set new pin configuration for signal
+ * 
+ * @param signal address of signal instance
+ * @param config new pin configuration
+ * @return uint8_t return 1 if signal added, 0 if there is no space
+ */
+void Signal_setConfig(Signal* signal, const Signal_PinConfig* config) {
+    signal->Config = config;
+}
+/**
+ * @brief get signal pin config
+ * 
+ * @param signal 
+ * @return const Signal_PinConfig* 
+ */
+const Signal_PinConfig* Signal_getConfig(Signal* signal) {
+    return signal->Config;
+}
+#if SIGNAL_MAX_NUM > 0
+/**
+ * @brief finding a empty space for new signal in array
+ * 
+ * @return Signal* return null if not found empty space
+ */
+Signal* Signal_new(void) {
+    uint8_t len = SIGNAL_MAX_NUM;
+    Signal* pSignal = signals;
+    while (len-- > 0) {
+        if (!pSignal->Configured) {
+            return pSignal;
+        }
+        pSignal++;
+    }
+    return SIGNAL_NULL;
+}
+#endif // SIGNAL_MAX_NUM
+/**
+ * @brief add signal into list for process
+ * 
+ * @param signal address of signal
+ * @param config signal pin configuration
+ */
+uint8_t Signal_add(Signal* signal, const Signal_PinConfig* config) {
+    // check for null
+    if (SIGNAL_NULL == signal) {
+        return 0;
+    }
+    //Signal_State stateT;
+    
+    //stateT = (Signal_State) (((stateT << 1) | signalDriver->readPin(signal->Config)) & 0x03);
+    // add new signal to list
+    
+    //signal->NotActive = Signal_NotHandled;
+    Signal_setConfig(signal, config);
+    // init IOs
+    signalDriver->initPin(config);
+    signal->State = (signalDriver->readPin(signal->Config) << 1 | signalDriver->readPin(signal->Config)) & 3;
+#if SIGNAL_MAX_NUM == -1
+    // add signal to linked list
+    signal->Previous = lastSignal;
+    lastSignal = Signal;
+#endif // SIGNAL_MAX_NUM == -1
+    signal->Configured = 1;
+    signal->Enabled = 1;
+    return 1;
+}
+/**
+ * @brief remove signal from list
+ * 
+ * @param remove address of signal
+ * @return uint8_t return 1 if signal found, 0 if not found
+ */
+uint8_t Signal_remove(Signal* remove) {
+#if SIGNAL_MAX_NUM == -1
+    Signal* pSignal = lastSignal;
+    // check last signal first
+    if (remove == pSignal) {
+        // deinit IO
+    #if SIGNAL_USE_DEINIT
+        if (signalDriver->deinitPin) {
+            signalDriver->deinitPin(remove->Config);
+        }
+    #endif
+        // remove signal dropped from link list
+        pSignal->Previous = remove->Previous;
+        remove->Previous = SIGNAL_NULL;
+        remove->Configured = 0;
+        remove->Enabled = 0;
+        return 1;
+    }
+    while (SIGNAL_NULL != pSignal) {
+        if (remove == pSignal->Previous) {
+            // deinit IO
+		#if SIGNAL_USE_DEINIT
+            if (signalDriver->deinitPin) {
+                signalDriver->deinitPin(remove->Config);
+            }
+        #endif
+            // remove signal dropped from link list
+            pSignal->Previous = remove->Previous;
+            remove->Previous = SIGNAL_NULL;
+            remove->Configured = 0;
+            remove->Enabled = 0;
+            return 1;
+        }
+        pSignal = pSignal->Previous;
+    }
+#else
+    uint8_t len = SIGNAL_MAX_NUM;
+    Signal* pSignal = signals;
+    while (len--) {
+        if (remove == pSignal && pSignal->Configured) {
+            pSignal->Configured = 0;
+            pSignal->Enabled = 0;
+            return 1;
+        }
+        pSignal++;
+    }
+#endif // SIGNAL_MAX_NUM == -1
+    return 0;
+}
+/**
+ * @brief finding signal based on PinConfig in list
+ * 
+ * @param config 
+ * @return Signal* 
+ */
+Signal* Signal_find(const Signal_PinConfig* config) {
+#if SIGNAL_MAX_NUM == -1
+    Signal* pSignal = lastSignal;
+    while (SIGNAL_NULL != pSignal) {
+        if (config == pSignal->Config) {
+            return pSignal;
+        }
+        pSignal = pSignal->Previous;
+    }
+#else
+    uint8_t len = SIGNAL_MAX_NUM;
+    Signal* pSignal = signals;
+    while (len--) {
+        if (config == pSignal->Config) {
+            return pSignal;
+        }
+        pSignal++;
+    }
+#endif // SIGNAL_MAX_NUM == -1
+    return SIGNAL_NULL;
+}
+
+#if SIGNAL_MULTI_CALLBACK
+void Signal_onLow(Signal* signal, Signal_Callback cb) {
+    signal->Callbacks.onLow = cb;
+}
+void Signal_onRising(Signal* signal, Signal_Callback cb) {
+    signal->Callbacks.onRising = cb;
+}
+void Signal_onFailing(Signal* signal, Signal_Callback cb) {
+    signal->Callbacks.onFailing = cb;
+}
+
+void Signal_onHigh(Signal* signal, Signal_Callback cb) {
+    signal->Callbacks.onHigh = cb;
+}
+
+#else
+void Signal_onChange(Signal* signal, Signal_Callback cb) {
+    Signal->Callbacks.onChange = cb;
+}
+#endif // SIGNAL_MULTI_CALLBACK
+
+#if SIGNAL_ACTIVE_STATE
+void Signal_setActiveState(Signal* signal, Signal_ActiveState state) {
+    signal->ActiveState = (uint8_t) state;
+}
+Signal_ActiveState Signal_getActiveState(Signal* signal) {
+    return (Signal_ActiveState) signal->ActiveState;
+}
+#endif /* SIGNAL_ACTIVE_STATE_ENABLE */
+
+#if SIGNAL_ENABLE_FLAG
+void Signal_setEnabled(Signal* Signal, uint8_t enabled) {
+    signal->Enabled = enabled;
+}
+uint8_t Signal_isEnabled(Signal* signal) {
+    return signal->Enabled;
+}
+#endif
+
+#if SIGNAL_ARGS
+void Signal_setArgs(Signal*, void* args) {
+    Signal->Args = args;
+}
+void* Signal_getArgs(Signal* signal) {
+    return signal->Args;
+}
+#endif
+
---
+++ Src/Signal.h
@@ -0,0 +1,232 @@
+ /**
+ * @file Signal.h
+ * @author Ali Mirghasemi (ali.mirghasemi1376@gmail.com)
+ * @brief this library use for drive Signal, button and input signals
+ * @version 0.2.0
+ * @date 2021-06-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef _SIGNAL_H_
+#define _SIGNAL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+#include <stdint.h>
+
+/******************************************************************************/
+/*                                Configuration                               */
+/******************************************************************************/
+
+/**
+ * @brief define SIGNAL_MULTI_CALLBACK if u want have sperate callback functions
+ * for each state such as Signal_onFailing, Signal_onLow, Signal_onRising, Signal_onHigh
+ */
+#define SIGNAL_MULTI_CALLBACK              1
+/**
+ * @brief if you enable this option you can disable or enable Signal
+ */
+#define SIGNAL_ENABLE_FLAG                 0
+/**
+ * @brief user must define deinitPin function in Signal_Driver
+ */
+#define SIGNAL_USE_DEINIT                  0
+/**
+ * @brief if Signal is based on pair of GPIO and Pin num must enable it
+ * for arduino must disable it
+ */
+#define SIGNAL_CONFIG_IO                   1
+/**
+ * @brief if the output of the callback <the events are executed 
+ * 
+ */
+#define SIGNAL_IDLE                        Signal_State_High
+
+/**
+ * @brief hold Signal io
+ * user can change it to GPIO_TypeDef or anything else that system want
+ */
+typedef void* Signal_IO;
+/**
+ * @brief hold Signal pin num or pin bit
+ * user can change it to uint8_t for 8-bit systems like AVR
+ */
+typedef uint16_t Signal_Pin;
+
+/**
+ * @brief maximum number of Signals
+ * -1 for unlimited, lib use linked list 
+ * x for limited Signals, lib use pointer array
+ */
+#define SIGNAL_MAX_NUM                     3
+
+/**
+ * @brief user can store some args in Signal struct and retrive them in callbacks
+ */
+#define SIGNAL_ARGS                        0
+
+/******************************************************************************/
+
+#define SIGNAL_NULL            ((Signal*) 0)
+#define SIGNAL_CONFIG_NULL     ((Signal_PinConfig*) 0)
+
+/**
+ * @brief hold pin configuration that use for handle Signal
+ */
+typedef struct {
+#if SIGNAL_CONFIG_IO
+    Signal_IO     	IO;
+#endif
+    Signal_Pin      Pin;
+} Signal_PinConfig;
+
+/**
+ * @brief Signal state
+ *     _______               ___(High)___
+ * (Failing)<-|____(Low)____|-> (Rising)
+ */
+typedef enum {
+    Signal_State_Low           = 0x00,
+    Signal_State_Rising        = 0x01,
+    Signal_State_Failing       = 0x02,
+    Signal_State_High          = 0x03
+} Signal_State;
+
+/**
+ * @brief show next callbacks can fire or ignore incoming callbacks
+ */
+typedef enum {
+    Signal_NotHandled           = 0,
+    Signal_Handled	            = 1
+} Signal_HandleStatus;
+
+struct _Signal;
+typedef struct _Signal Signal;
+
+/**
+ * @brief initialize pin in input mode, remember if your pin is pull-up, or pull-down 
+ * must configured in init function
+ * this function call when new Signal add to queue
+ */
+typedef void (*Signal_InitPinFn)(const Signal_PinConfig* config);
+/**
+ * @brief de-initialize pin and change pin to reset mode
+ * this function call on remove Signal
+ */
+typedef void (*Signal_DeInitPinFn)(const Signal_PinConfig* config);
+/**
+ * @brief this function must return value of a pin
+ * 0 -> LOW, 1 -> HIGH
+ */
+typedef uint8_t (*Signal_ReadPinFn)(const Signal_PinConfig* config);
+/**
+ * @brief this callback call when Signal state change
+ * 
+ * @param Signal show which Signal changed
+ * @param state show current state of Signal
+ * @return user can return Signal_NotHandled (0) if wanna get callback on other events 
+ *                  otherwise can return Signal_Handled (1) that mean Signal handled nad next event is onPressed
+ */
+typedef Signal_HandleStatus (*Signal_Callback)(Signal* signal, Signal_State state);
+/**
+ * @brief hold minimum function for Signal lib to work
+ * user must pass atleast init and read functions to Signal library
+ */
+typedef struct {
+    Signal_InitPinFn     initPin;
+    Signal_ReadPinFn     readPin;
+    #if Signal_USE_DEINIT
+        Signal_DeInitPinFn   deinitPin;
+    #endif
+} Signal_Driver;
+
+
+// determine how many callbacks need
+#if SIGNAL_MULTI_CALLBACK && Signal_NONE_CALLBACK
+    #define SIGNAL_CALLBACKS_NUM 4
+#elif SIGNAL_MULTI_CALLBACK
+    #define SIGNAL_CALLBACKS_NUM 4
+#else
+    #define SIGNAL_CALLBACKS_NUM 1
+#endif
+
+typedef union {
+    Signal_Callback            callbacks[SIGNAL_CALLBACKS_NUM];
+    struct {
+    #if SIGNAL_MULTI_CALLBACK
+        Signal_Callback        onLow;
+        Signal_Callback        onRising;
+        Signal_Callback        onFailing;
+        Signal_Callback        onHigh;
+    #else
+        Signal_Callback        onChange;
+    #endif // SIGNAL_MULTI_CALLBACK
+    };
+} Signal_Callbacks;
+
+/**
+ * @brief this struct hold Signal parameters
+ * do not change the parameters or something else with it
+ */
+struct _Signal {
+#if SIGNAL_MAX_NUM == -1
+    struct _Signal*            Previous;               			/**< point to previous Signal, if it's null show they Signal is end of linked list */
+#endif // SIGNAL_MAX_NUM == -1
+#if Signal_ARGS
+    void*                   Args;
+#endif
+    const Signal_PinConfig*    Config;                 			/**< hold pointer to pin configuration */
+    Signal_Callbacks           Callbacks;                          /**< hold user separate callbacks for each Signal state */
+    uint8_t                 State           : 2;    			/**< show current state of Signal*/
+    uint8_t                 NotActive       : 1;    			/**< show other states will be ignore or not */
+    uint8_t                 ActiveState     : 1;    			/**< this parameters use only when Activestate Enabled */
+    uint8_t                 Configured      : 1;                /**< this flag shows Signal is configured or not, just useful fo fixed Signal num */
+    uint8_t                 Enabled         : 1;                /**< check this flag in irq */
+    uint8_t                 Reserved        : 2;
+};
+
+void Signal_init(const Signal_Driver* driver);
+void Signal_handle(void);
+
+void Signal_setConfig(Signal* signal, const Signal_PinConfig* config);
+const Signal_PinConfig* Signal_getConfig(Signal* signal);
+
+#if SIGNAL_MAX_NUM > 0
+    Signal* Signal_new(void);
+#endif // SIGNAL_MAX_NUM
+
+uint8_t  Signal_add(Signal* signal, const Signal_PinConfig* config);
+uint8_t  Signal_remove(Signal* remove);
+Signal*  Signal_find(const Signal_PinConfig* config);
+
+#if SIGNAL_MULTI_CALLBACK
+    void Signal_onLow(Signal* signal, Signal_Callback cb);
+    void Signal_onRising(Signal* signal, Signal_Callback cb);
+    void Signal_onFailing(Signal* signal, Signal_Callback cb);
+    void Signal_onHigh(Signal* signal, Signal_Callback cb);
+#else
+    void Signal_onChange(Signal* Signal, Signal_Callback cb);
+#endif // SIGNAL_MULTI_CALLBACK
+
+
+
+
+#if SIGNAL_ENABLE_FLAG
+    void Signal_setEnabled(Signal* signal, uint8_t enabled);
+    uint8_t Signal_isEnabled(Signal* signal);
+#endif
+
+#if Signal_ARGS
+    void  Signal_setArgs(Signal*, void* args);
+    void* Signal_getArgs(Signal* signal);
+#endif
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* _SIGNAL_H_ */
